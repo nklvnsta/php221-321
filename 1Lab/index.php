06abf6bba3d4a68a51d8fb83e1eef8f0e6c373c0
Anastasia <nastanikol10105@gmail.com>
2023-02-21 21:51:13 +0300

    1Lab

---
+++ 1Lab/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>1 lab</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <!-- <img src="2023-02-16_17-04-27.png" alt=""> -->
+        <?php
+                echo "<img src='2023-02-16_17-04-27.png'>";
+            ?>
+        <h1>
+            <?php
+                echo "2.1.Домашняя работа: Hello, World!";
+            ?>
+        </h1>
+    </header>
+    <main>      
+        <div>
+            <?php
+                echo "Привет, я - скрипт PHP!";
+            ?>
+        </div> 
+    </main>
+    <footer>
+        <p>
+        <?php
+                echo "Николаева Анастасия группа 221-321";
+            ?>
+        </p>
+    </footer>
+
+</body>
+</html>
